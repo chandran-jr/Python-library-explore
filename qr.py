32e68f84a36b0afda3fd41993f6ea7c4ef67625c
Govind B Chandran <govindchandran235@gmail.com>
2020-06-02 21:22:39 +0530

    Delete qr.py

--- qr.py
+++
@@ -1,4 +0,0 @@
-import qrcode
-
-qr= qrcode.make('Hello this is Govind B Chandran and heres a qr code i have made using this very simple code and the qrcode module.Happy coding :)')
-qr.save('myQR.png')

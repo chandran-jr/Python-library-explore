f89c8a6893f5e1a7c0ef5c3602ba37f290dd8e51
Govind B Chandran <govindchandran235@gmail.com>
2020-06-02 21:16:52 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,4 @@
+import qrcode
+
+qr= qrcode.make('Hello this is Govind B Chandran and heres a qr code i have made using this very simple code and the qrcode module.Happy coding :)')
+qr.save('myQR.png')

2ed4ae247958b88a0c81db9b9ceed20ee6a57cc5
Govind B Chandran <govindchandran235@gmail.com>
2020-06-22 10:18:03 +0530

    Convert speech to text

---
+++ speech2text.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak now:")
+    audio = r.listen(source)
+    
+    print(r.recognize_google(audio))

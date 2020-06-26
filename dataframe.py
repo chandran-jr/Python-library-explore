58ae3e211e74835882ef47420e59206da3cb1706
Govind B Chandran <govindchandran235@gmail.com>
2020-06-27 01:22:16 +0530

    Data Frames using pandas

---
+++ dataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data={'Name':['Ben','Govind','Mannu'],
+      'Age':['22','24','36'],
+      'Job':['Developer','Scientist','Designer'],
+      'Place':['Tvm','Canada','Uganda']}
+      
+df = pd.DataFrame(data)
+print(df)

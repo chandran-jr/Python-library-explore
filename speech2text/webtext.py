af1410dfd9f1c69f37ab8351a05066a63c1d9ea5
Govind B Chandran <govindchandran235@gmail.com>
2020-07-04 02:28:03 +0530

    Add files via upload

---
+++ speech2text/webtext.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 11:55:12 2020
+
+@author: govin
+"""
+
+import speech_recognition as sr
+import webbrowser as wb
+import speak
+
+chrome_path =  'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print ('Say Something!')
+    audio = r.listen(source)
+    print ('Done!')
+ 
+try:
+    text = r.recognize_google(audio)
+    print('Google thinks you said:\n' + text)
+    lang = 'en'
+
+    speak.tts(text, lang)
+
+    f_text = 'https://www.google.co.in/search?q=' + text
+    wb.get(chrome_path).open(f_text)
+ 
+except Exception as e:
+    print (e)

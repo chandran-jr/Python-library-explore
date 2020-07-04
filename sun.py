232d2c865bf8ef793c990442b99e083eb3e6cbc2
Govind B Chandran <govindchandran235@gmail.com>
2020-07-05 00:54:27 +0530

    Drawing a sun using turtle

---
+++ sun.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.color("red","yellow")
+turtle.begin_fill()
+turtle.speed(10)
+for i in range(50):
+    turtle.forward(300)
+    turtle.left(170)
+turtle.end_fill()
+turtle.done()
